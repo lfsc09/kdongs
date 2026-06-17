7c2bdb9625cdcd096add7db48a0d73e118faddd0
lfsc09 <lfsc09@gmail.com>
2026-06-17 09:35:26 -0300

    refactor: change user id to uuid

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use Override;
 use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use Override;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -50,5 +51,7 @@ class AppServiceProvider extends ServiceProvider
                 ->uncompromised()
             : null,
         );
+
+        Model::automaticallyEagerLoadRelationships();
     }
 }
